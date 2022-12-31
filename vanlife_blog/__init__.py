699a851b722db45b5f485ae122b03d37846a8584
Axel Zwaans <axelzwaans@hotmail.com>
2022-12-31 09:20:53 +0000

    Fix typo error for connecting db

--- vanlife_blog/__init__.py
+++ vanlife_blog/__init__.py
@@ -14,9 +14,9 @@ if os.environ.get("DEVELOPMENT") == "True":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
     uri = os.environ.get("DATABASE_URL")
-    if uri.startswith("postgress://"):
-        uri = uri.replace("postgress://", "postgresql://", 1)
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
 db = SQLAlchemy(app)
 
