3a80d830865a6f85900d8bb4a1c4bbc810591136
Axel Zwaans <axelzwaans@hotmail.com>
2023-01-04 19:44:13 +0000

    Finish edit user functionality

--- vanlife_blog/routes.py
+++ vanlife_blog/routes.py
@@ -107,6 +107,7 @@ def delete_comment(comment_id):
     if not comment:
         flash('Comment does not exist', 'error')
     else:
+        flash('Comment deleted', 'success')
         db.session.delete(comment)
         db.session.commit()
     
@@ -122,10 +123,10 @@ def like(post_id):
 
     if not post:
         return jsonify({'error': 'Post does not exist.'}, 400)
-    elif like:
+    elif like:       
         db.session.delete(like)
         db.session.commit()
-    else:
+    else:        
         like = Like(author=current_user.id, post_id=post_id)
         db.session.add(like)
         db.session.commit()
@@ -138,35 +139,40 @@ def like(post_id):
 @login_required
 def dashboard():
     app_users = User.query.order_by(User.date_created)
-    return render_template('dashboard.html', app_users=app_users)
+    admin = current_user.id
+
+    return render_template('dashboard.html', app_users=app_users, admin=admin)
 
 
 # Edit User
 @app.route('/edit_user/<id>', methods=['GET', 'POST'])
 @login_required
 def edit_user(id):
-    username_to_update = User.query.get_or_404(id)
-    print("/n/n---++++----/n/n")
-    print(username_to_update)
-    
+    username_to_update = User.query.filter_by(id=id).first()
+
     if request.method == 'POST':
-        username_to_update.username = request.form['username']
-        try:
+        text = request.form.get('username')
+        username = request.form.get('username')
+        username_exists = User.query.filter_by(username=username).first()
+
+        if not text:
+            flash('Username cannot be empty', 'error')
+        elif username_exists:  
+            flash('Username is already in use', 'error')
+        else:  
+            username_to_update.username = request.form['username']
             db.session.commit()
             flash('Username updated', 'success')
-            return render_template('edit_user.html', username_to_update=username_to_update)
-        except:
-            flash('Unable to edit user', 'error')
-            return render_template('edit_user.html', username_to_update=username_to_update)
-    else:
-        return render_template('edit_user.html', username_to_update=username_to_update)
+            return redirect(url_for('dashboard'))
+
+    return render_template('edit_user.html', username_to_update=username_to_update)
 
 
 # Delete user
-@app.route("/delete_user/user_id", methods=['GET', 'POST'])
+@app.route("/delete_user/id", methods=['GET', 'POST'])
 @login_required
 def delete_user(user_id):
-    user = User.query.filter_by(id=id).first()
+    user_to_delete = User.query.filter_by(id=id).first()
 
     if not post:
         flash("User does not exist", 'error')
